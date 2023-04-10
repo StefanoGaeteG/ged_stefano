292c252eacebe35a20f89cb19e0521504323eb35
stefanogaeteg <franciscos.gaeteg@gmail.com>
2023-04-10 13:54:54 -0400

    0.3

--- index.php
+++ index.php
@@ -1,19 +1,12 @@
-<?php 
-echo "hola mundo";
-phpinfo();
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <?php include_once "head.php" ?>
 </head>
 <body>
-    <h>hola</h>
-    <p>este es un parafo</p>
+    <div class="menu">
+        <nad><input type="text"  name="" id=""><input type="button" value="buscar"></nad>
+    </div>
 </body>
 </html>
 
